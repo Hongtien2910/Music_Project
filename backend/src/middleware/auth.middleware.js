be3c6ca28d3acbeb414b23199e8eb7fee7b77c97
Võ Thị Hồng Tiên <hongtien003@gmail.com>
2025-04-24 17:21:34 +0700

    protect route

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectRoute = (req, res, next) => {
+    if (!req.auth.userId) {
+        res.status(401).json({ message: "Unauthorized - you must be logged in" });  
+        return;
+    }
+    next();
+};
+
+export const requireAdmin = async (req, res, next) => {
+    try {
+        const currentUser = await clerkClient.users.getUser(req.auth.userId);
+        const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress.emailAddress;
+        emailAddress;
+
+        if (!isAdmin) {
+            return res.status(403).json({ message: "Forbidden - you must be an admin" });
+        }
+        next();
+    } catch (error) {
+    }
+}
