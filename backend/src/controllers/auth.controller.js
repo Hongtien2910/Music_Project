df85a34e47bf283e87e48839d3ad39e46048066d
Võ Thị Hồng Tiên <hongtien003@gmail.com>
2025-04-24 16:50:54 +0700

    Basic

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user.model.js";
+
+export const authCallback = async (req, res) => {
+    try {
+        const { id, firstName, lastName, imageUrl} = req.body;
+    
+        // check if user already exists
+        const user = await User.findOne({ clerkId: id });
+        if (!user) {
+          //sign up
+          await User.create({
+            fullName: `${firstName} ${lastName}`,
+            imageUrl: imageUrl,
+            clerkId: id,
+          });
+        }
+        res.status(200).json({success: true});
+    } catch (error) {
+        console.error("Error in callback:", error);
+        return res.status(500).json({ message: "Internal server error", error });
+    }
+}
