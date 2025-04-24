1b41b01ed24f07b7870166b67ecca4204abaa388
Võ Thị Hồng Tiên <hongtien003@gmail.com>
2025-04-24 17:22:25 +0700

    basic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+function App() {
+  return (
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header> 
+    </>
+  )
+} 
+export default App
