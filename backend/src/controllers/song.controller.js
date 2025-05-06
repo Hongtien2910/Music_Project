2bf7fd6c661172ad37834e4f25c11a5f92e8037a
Võ Thị Hồng Tiên <hongtien003@gmail.com>
2025-05-06 14:04:58 +0700

    backend album and song

---
+++ backend/src/controllers/song.controller.js
@@ -0,0 +1,85 @@
+import { Song } from "../models/song.model.js";
+
+export const getAllSongs = async (req, res, next) => {
+    try {
+        // -1 means descending order, 1 means ascending order
+        const songs = await Song.find().sort({createAt: -1});
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getAllSongs", error);
+        next(error);
+    }
+};
+
+export const getFeaturedSongs = async (req, res, next) => {
+    // 6 random songs use mongodb aggregate pipeline
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample:{size:6}
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                    lyricUrl: 1,
+                }
+            }
+        ])
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getFeaturedSongs", error);
+        next(error);
+    }
+};
+
+export const getMadeForYouSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample:{size:4}
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                    lyricUrl: 1,
+                }
+            }
+        ])
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getMadeForYouSongs", error);
+        next(error);
+    }
+};
+
+export const getTrendingSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample:{size:4}
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                    lyricUrl: 1,
+                }
+            }
+        ])
+        res.status(200).json(songs);
+    } catch (error) {
+        console.log("Error in getTrendingSongs", error);
+        next(error);
+    }
+};
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controllers/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { get } from "mongoose";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Song route with get method");
-});
+router.get("/", protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
+
 
 export default router;
