386626e3f1ff89725fec65c9417fe9b30e1076b4
Võ Thị Hồng Tiên <hongtien003@gmail.com>
2025-06-19 14:56:53 +0700

    bonus recommend

--- Recommend/generate_features.py
+++ Recommend/generate_features.py
@@ -1,10 +1,14 @@
-from recommend_CNN import load_data, build_model, save_feature_vectors
+from recommend_CNN import load_data, build_model, save_feature_vectors, get_feature_extractor
 import torch
-# Load model
+
+# Load full model
 model = build_model()
 model.load_state_dict(torch.load("model_CNN.pt", map_location=torch.device("cpu")))
 model.eval()
 
-# Load dữ liệu ảnh và lưu các vector đặc trưng
+# Cắt lớp cuối để trích đặc trưng (bỏ Linear(40→8))
+feature_model = get_feature_extractor(model)
+
+# Load dữ liệu ảnh và lưu vector đặc trưng
 images_by_label = load_data()
-save_feature_vectors(images_by_label, model, "features.pkl")
+save_feature_vectors(images_by_label, feature_model, "features.pkl")
